e2a6945dd60f06c58ca04428368307d18006c237
Hiroki Kato <hkato@sas.upenn.edu>
2018-01-30 04:08:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,40 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.find do |win_combo|
+    board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]] && position_taken?(board, win_combo[0])
+  end
+end
+
+  def full?(board)
+  board.all? do |combo|
+    if combo == "X" || combo == "O"
+      true
+    end
+  end
+end
+
+def draw?(board)
+  (!won?(board) && full?(board))?  true : false
+end
+
+def over?(board)
+  (won?(board) || draw?(board) || full?(board))? true : false
+end
+
+def winner(board)
+  winner_token = won?(board)
+  (winner_token)? board[winner_token[0]] : nil
+end
